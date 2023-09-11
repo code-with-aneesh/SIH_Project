f131d11386c52b58be4c1076c77c37a30ea132e9
Pawansinghrajpoot <143216683+Pawansinghrajpoot@users.noreply.github.com>
2023-09-11 15:31:10 +0530

    Update App.js

--- project/src/App.js
+++ project/src/App.js
@@ -2,12 +2,14 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Register from './components/register/register';
+import Login from './components/register/Login';
 
 function App() {
   return (
   
       <Routes>
         <Route path="/" element={<Register/>} />
+        <Route path="/L" element={<Login/>} />
         {/* Add more routes for other pages */}
       </Routes>
    
